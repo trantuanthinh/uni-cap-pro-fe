c6472b4bd2492d36689df33c83202bba48667a17
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2024-08-04 15:15:50 +0700

    commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,25 @@
+// pages/_document.js
+
 import Footer from "@/components/Footer/footer";
 import Header from "@/components/Header/header";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-    // const user = {
-    //     username: "trantuanthinh",
-    //     age: 22,
-    // };
-
     const user = null;
+
     return (
         <Html lang="en">
-            <Head />
-            <Header user={ user } />
+            <Head>
+                <title>Capstone Project</title>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
             <body>
+                <Header user={ user } />
                 <Main />
                 <NextScript />
+                <Footer />
             </body>
-            <Footer />
         </Html>
     );
 }
