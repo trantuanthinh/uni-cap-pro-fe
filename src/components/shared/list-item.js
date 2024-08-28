6254b2e94fbf8117c6f52a89185ba1bae76d7c27
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2024-08-28 17:02:02 +0700

    fixed

--- src/components/shared/list-item.js
+++ src/components/shared/list-item.js
@@ -4,9 +4,9 @@ export default function ListItem({ productList, pageSize, type }) {
     return (
         <div className="grid grid-cols-4 grid-flow-row">
             { productList &&
-                productList.slice(0, pageSize).map((product) => {
-                    return <ItemCard key={ product.id } id={ product.id } product={ product } type={ type } />;
-                }) }
+                productList.slice(0, pageSize).map((product) => (
+                    <ItemCard key={ product.id } id={ product.id } product={ product } type={ type } />
+                )) }
         </div>
     );
 }
