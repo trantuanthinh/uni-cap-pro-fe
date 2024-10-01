f1433c4ca9968d9de9b4de033589eed69b771f02
Trần Tuấn Thịnh <tran.tuan.thinh.0125@gmail.com>
2024-10-01 18:40:56 +0700

    update pagination

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,5 +1,7 @@
 import ListItem from "@/components/shared/list-item";
 import apiService from "@/services/api-service";
+import sharedService from "@/services/sharedService";
+import { Pagination } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -7,30 +9,42 @@ export default function Products() {
     const router = useRouter();
     const { Page } = router.query;
     const [ list, setList ] = useState([]);
+    const pageSize = 8; // Define pageSize or pass it as a prop if needed
 
     useEffect(() => {
         if (router.isReady) {
-            getAllProducts();
+            const option = sharedService.isNullOrEmpty(Page) ? {} : { page: Page };
+            apiService
+                .getProducts(option)
+                .then((productRes) => {
+                    setList(productRes.result.data);
+                })
+                .catch((error) => {
+                    console.log("Error: ", error);
+                });
         }
     }, [ router.isReady, Page ]);
 
-    function getAllProducts() {
-        let option = { page: Page || 1 };
-        apiService
-            .getProducts(option)
-            .then((productRes) => {
-                setList(productRes.result.data);
-            })
-            .catch((error) => {
-                console.log("Error: ", error);
-            });
+    function handlePageChange(newPage) {
+        router.push({
+            pathname: router.pathname,
+            query: { ...router.query, Page: newPage },
+        });
     }
 
     return (
         <>
-            <div className="max-w-screen-xl mx-auto py-6">
-                <h1 className="text-3xl text-center font-bold mb-4">All Products</h1>
-                <ListItem productList={ list } />
+            <div className="flex flex-col items-center justify-center max-w-screen-xl mx-auto py-6">
+                <h1 className="text-3xl font-bold mb-4">All Products</h1>
+                <ListItem productList={ list } pageSize={ pageSize } />
+                <Pagination
+                    loop
+                    showControls
+                    color="success"
+                    total={ 10 }
+                    page={ Number(Page) || 1 }
+                    onChange={ handlePageChange }
+                />
             </div>
         </>
     );
