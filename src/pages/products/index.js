6bafa48e8055e0205d0288d6a04c1f09e93c8c2e
Trần Tuấn Thịnh <tran.tuan.thinh.0125@gmail.com>
2024-11-18 23:56:33 +0700

    [feat] search product UI, todo: func

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -2,19 +2,41 @@ import ListItem from "@/components/shared/list-item";
 import Title from "@/components/shared/title";
 import GlobalSettings from "@/configurations/global-settings";
 import apiService from "@/services/api-service";
-import { Input, Pagination } from "@nextui-org/react";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Pagination } from "@nextui-org/react";
 import { debounce } from "lodash";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 import { toast } from "sonner";
 
 export default function Products() {
+    const items = [
+        {
+            key: "new",
+            label: "New file",
+        },
+        {
+            key: "copy",
+            label: "Copy link",
+        },
+        {
+            key: "edit",
+            label: "Edit file",
+        },
+        {
+            key: "delete",
+            label: "Delete file",
+        },
+    ];
     const router = useRouter();
     const { Page, CategoryId } = router.query;
+
+    const currentPage = Number(Page) || 1;
     const [list, setList] = useState([]);
+    const [isAscPrice, setIsAscPrice] = useState(true);
     const [totalPages, setTotalPages] = useState(0);
     const [pageSize, setPageSize] = useState(0);
-    const currentPage = Number(Page) || 1;
     const [search, setSearch] = useState("");
 
     // Fetch products when the router is ready and Page/CategoryId/search changes
@@ -56,7 +78,6 @@ export default function Products() {
         }
     }, [router.isReady, currentPage, CategoryId, search]);
 
-
     useEffect(() => {
         if (totalPages > 0 && currentPage > totalPages) {
             router.push({
@@ -73,7 +94,7 @@ export default function Products() {
         });
     }
 
-    const handleChange = (input) => {
+    const onSubmit = (input) => {
         setSearch(input);
 
         const query = { ...router.query };
@@ -94,13 +115,39 @@ export default function Products() {
             <Title label={`${ GlobalSettings.Settings.name } - Products`} />
             <div className="flex flex-col items-center justify-center max-w-screen-xl mx-auto py-6">
                 <h1 className="text-3xl font-bold mb-4">All Products</h1>
-                <Input
-                    label="Search Products' Name"
-                    type="text"
-                    id="search"
-                    value={search}
-                    onChange={(e) => handleChange(e.target.value)}
-                />
+
+                <form
+                    className="flex flex-row w-full justify-center items-center space-x-4"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        toast.success("Success!");
+                    }}>
+                    <div className="flex flex-col space-y-2">
+                        <Button className="text-base font-medium" onClick={() => setIsAscPrice(!isAscPrice)}>
+                            Price {isAscPrice ? <FaArrowUp size={14} /> : <FaArrowDown size={14} />}
+                        </Button>
+
+                        <Dropdown>
+                            <DropdownTrigger>
+                                <Button color="primary" className="text-base font-medium">
+                                    Category <IoIosArrowDown size={14} />
+                                </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu aria-label="Dynamic Actions" items={items}>
+                                {(item) => <DropdownItem key={item.key}>{item.label}</DropdownItem>}
+                            </DropdownMenu>
+                        </Dropdown>
+                    </div>
+
+                    <Input
+                        className="w-[80%]"
+                        label="Search Products' Name"
+                        type="text"
+                        id="search"
+                        value={search}
+                        onChange={(e) => onSubmit(e.target.value)}
+                    />
+                </form>
 
                 <ListItem list={list} pageSize={pageSize} type="product" id="search" />
                 {totalPages > 1 && (
